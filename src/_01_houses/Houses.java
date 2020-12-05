0e9f5a2e8dcb49ead4889b2366ad7c12442ea99a
root <root@cdc52cec7df1>
2020-12-05 22:31:39 +0000

    working on houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,27 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
-	public void run() {
+	static Robot robot = new Robot();
+
+	
+	public static void main(String[] args) {
+		drawHouse();
+		robot.penDown();
+		robot.setPenColor(0,255,0);
+		
+		
+			
+	}
+	public static void drawHouse() {
+		
+		
+		
 		
 	}
+	
+	
+	
+	
 }
