6b525c57044b37b215dd99e4df36bc910ddc35cb
root <root@e4fc757cb1ec>
2020-12-12 22:24:58 +0000

    working

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,27 +1,72 @@
 package _01_houses;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
 	static Robot robot = new Robot();
 
 	
-	public static void main(String[] args) {
-		drawHouse();
+	public void setUp(){
+
 		robot.penDown();
 		robot.setPenColor(0,255,0);
-		
+		robot.miniaturize();
+		robot.setSpeed(50);
 		
 			
 	}
-	public static void drawHouse() {
-		
-		
-		
+	public void runProgram(){
+		for (int i = 0; i < 10; i++) {
+			drawLargeHouse();
+			robot.turn(-90);
+			robot.setPenColor(0,255,0);
+			robot.move(20);
+			robot.turn(-90);
+		}
 		
 	}
+	private void drawBuiding() {
+	String input = JOptionPane.showInputDialog("Would you like a small, medium or large building?");
+	
+	if(input.equalsIgnoreCase("small")) {
+		drawSmallHouse();
+	}
+	
+	else if(input.equalsIgnoreCase("medium")) {
+		drawMediumHouse();
+	}
+	else {
+		drawLargeHouse();
+	}
+		
+	}
+
+	public static void drawLargeHouse() {
+		
+		robot.move(100);
+		robot.turn(90);
+		robot.move(20);
+		robot.turn(90);
+		robot.move(100);
 	
 	
+}
+public static void drawMediumHouse() {
+		
+		robot.move(75);
+		robot.turn(90);
+		robot.move(20);
+		robot.turn(90);
+		robot.move(75);
+}
+public static void drawSmallHouse() {
 	
-	
+	robot.move(50);
+	robot.turn(90);
+	robot.move(20);
+	robot.turn(90);
+	robot.move(50);
+}
 }
--- src/_01_houses/HousesRunner.java
+++ src/_01_houses/HousesRunner.java
@@ -5,7 +5,8 @@ public class HousesRunner {
 	public static void main(String[] args) {
 		//Here we call the run() method from the Houses class
 		Houses houseBuilder = new Houses();
-		houseBuilder.run();
+		houseBuilder.setUp();
+		houseBuilder.runProgram();
 
 
 	}
