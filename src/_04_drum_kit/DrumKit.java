a9d8ee20cd5379747340366dfac5c163032b060d
root <root@09f8409a03cf>
2021-01-10 20:56:25 +0000

    work in proggress

--- src/_04_drum_kit/DrumKit.java
+++ src/_04_drum_kit/DrumKit.java
@@ -25,35 +25,39 @@ public class DrumKit implements MouseListener {
 	public void run() throws MalformedURLException {
 
 		// 1. Make a JFrame variable and initialize it using "new JFrame()"
-
+		JFrame Frame = new JFrame();
 		// 2. Make the frame visible and
 		// set its default close operation to JFrame.EXIT_ON_CLOSE
-
+		Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Frame.setVisible(true);
 		// 3. Set the size of the frame
-
+		Frame.setSize(500,500);
+		
 		// 4. Set the title of the frame
-
+		Frame.setTitle("Drum Kit");
 		// 5. Make a JPanel variable and initialize it using "new JPanel().
-
+		JPanel comp = new JPanel();
+		
+		
 		// 6. Add the panel to the frame. (The panel is invisible.)
-
+		Frame.add(comp);
 		// 7. Download an image of a drum from the Internet. Drop it into your
 		// Eclipse project under "recipe package(drum_kit)".
-
+		String image = "drum.jpg";
 		// 8. Put the name of your image file in a String variable.
-
+		
 		// 9. Edit the next line to use your String variable
 		// drumLabelWithImage = createLabelImage(drumImageString);
-
+		drumLabelWithImage = createLabelImage(image);
 		// 10. Add the image to the panel
-
+		comp.add(drumLabelWithImage);
 		// 11. Set the layout of the panel to "new GridLayout()"
-
+		comp.setLayout(new GridLayout());
 		// 12. call the pack() method on the frame. Run your program. Do you see
 		// your drum image?
-
+		Frame.pack();
 		// 13. add this mouse listener to drumLabelWithImage
-
+		drumLabelWithImage.addMouseListener(this);
 		// 18. Add more images to make a drumkit. Remember to add this mouse
 		// listener to each one.
 
@@ -64,7 +68,7 @@ public class DrumKit implements MouseListener {
 		// the console to see when this is printed.
 
 		JLabel drumClicked = (JLabel) e.getSource(); // This line gets the label
-														// that the mouse
+		System.out.println("click");												// that the mouse
 														// clicked on
 
 		// 15. Download a drum sound and drop it into your "recipe package(drum_kit)".
