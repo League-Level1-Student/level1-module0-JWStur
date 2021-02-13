5336170bbada1f5bcb09957c485f741a7b0b5d60
root <root@b3024bef7d69>
2021-02-13 22:22:16 +0000

    Working on fortune cookie

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -1,9 +1,22 @@
 package _07_fortune_cookie;
 
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class FortuneCookie {
 	
     public void showButton() {
         System.out.println("Button clicked");
-   }
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        JButton button = new JButton();
+        frame.add(button);
+        frame.pack();
+        button.addbuttonlistener(this);
+        
+        
+   } 
     
 }
