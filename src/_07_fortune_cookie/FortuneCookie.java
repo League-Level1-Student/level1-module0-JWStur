4bfc5351b7566cf5feb96051ab1ea7a8f70a73aa
student <student@ip-172-31-59-113.us-west-2.compute.internal>
2021-02-20 14:24:21 -0800

    Working on Jack-in-the-box

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -1,11 +1,14 @@
 package _07_fortune_cookie;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-public class FortuneCookie {
+public class FortuneCookie implements ActionListener {
 	
     public void showButton() {
         System.out.println("Button clicked");
@@ -14,9 +17,31 @@ public class FortuneCookie {
         JButton button = new JButton();
         frame.add(button);
         frame.pack();
-        button.addbuttonlistener(this);
+        button.addActionListener(this); 
         
         
-   } 
+   }
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		JOptionPane.showMessageDialog(null, "Woohoo!");
+		int rand = new Random().nextInt(5);
+		
+		if (rand == 0) {
+			JOptionPane.showMessageDialog(null, "You Will Win the Lotery");
+		}
+		else if (rand == 1) {
+			JOptionPane.showMessageDialog(null, "You will live a long life");
+		}
+		else if (rand == 2) {
+			JOptionPane.showMessageDialog(null, "You will own a dog one day");
+		}
+		else if (rand == 3) {
+			JOptionPane.showMessageDialog(null, "You will own a cat one day");
+		}
+		else if (rand == 4) {
+			JOptionPane.showMessageDialog(null, "You will slip and fall into a puddle");
+		}
+	} 
     
 }
