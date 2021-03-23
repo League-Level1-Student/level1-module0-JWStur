d83dadfb7e20013f905e3857712211a76a7ca038
student <student@ip-172-31-58-107.us-west-2.compute.internal>
2021-03-22 19:56:52 -0700

    wip

--- src/_09_sound_effects_machine/sound_effects_machine.java
+++ src/_09_sound_effects_machine/sound_effects_machine.java
@@ -1,5 +1,62 @@
 package _09_sound_effects_machine;
 
+import java.io.File;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class sound_effects_machine {
 
+	public void displayButton() {
+		JFrame frame1 = new JFrame();
+        frame1.setVisible(true);
+        JButton button1 = new JButton();
+        button1.setText("Sound 1");
+        frame1.add(button1);
+        frame1.pack();
+        JFrame frame2 = new JFrame();
+        frame2.setVisible(true);
+        JButton button2 = new JButton();
+        button2.setText("Sound 2");
+        frame2.add(button2);
+        frame2.pack();
+        JFrame frame3 = new JFrame();
+        frame3.setVisible(true);
+        JButton button3 = new JButton();
+        button3.setText("Sound 3");
+        frame3.add(button3);
+        frame3.pack();
+        JFrame frame4 = new JFrame();
+        frame4.setVisible(true);
+        JButton button4 = new JButton();
+        button4.setText("Sound 4");
+        frame4.add(button4);
+        frame4.pack();
+       
+		
+	}
+	
+	private void playSound(String soundFile) {
+		String path = "src/_09_sound_effects_machine/";
+			File sound = new File(path+soundFile);
+			if (sound.exists()) {
+				new Thread(() -> {
+				try {
+					Clip clip = AudioSystem.getClip();
+					clip.open(AudioSystem.getAudioInputStream(sound));
+					clip.start();
+					Thread.sleep(clip.getMicrosecondLength()/1000);
+				}
+				catch (Exception e) {
+					System.out.println("Could not play this sound");
+				}}).start();
+	 		}
+			else {
+				System.out.println("File does not exist");
+			}
+		
+	}
+    
 }
--- src/_09_sound_effects_machine/sound_effects_machine_runner.java
+++ src/_09_sound_effects_machine/sound_effects_machine_runner.java
@@ -1,11 +1,10 @@
 package _09_sound_effects_machine;
 
-import _08_jack_in_the_box.jack_in_the_box;
 
 public class sound_effects_machine_runner {
 	public static void main(String[] args) {
 		sound_effects_machine machine = new sound_effects_machine();
-		machine.();
+		machine.displayButton();
 	}
 
 }
